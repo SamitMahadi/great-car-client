eabb0d093d7a1874da2ee08cf1490845a33a14ce
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-01 05:53:54 -0400

    firebase added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
 
 
 
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element: <Home></Home>
+            },
+            {
+                path:'/login',
+                element: <Login></Login>
+
             }
         ]
     }   
